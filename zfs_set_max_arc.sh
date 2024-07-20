e7fa8298520e3b9a369a9f16087e7806a67fb48c
geoai777 <4668088+geoai777@users.noreply.github.com>
2024-07-20 12:19:47 +0400

    Update zfs_set_max_arc.sh
    # 1.1 - reformated ifs and loops to experimental syntax, not yet consistent, but might look a bit better.
    # 1.1.1 - minor bugfix, added "q" to exit.

--- zfs_set_max_arc.sh
+++ zfs_set_max_arc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ZFS ARC set max memory script
+# ZFS set zfs_max_arc memory script
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -9,7 +9,10 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-version="1.0"
+# change log:
+# 1.1 - reformated ifs and loops to experimental syntax, not yet consistent, but might look a bit better.
+# 1.1.1 - minor bugfix, added "q" to exit.
+version="1.1.1"
 pve_dir="/etc/pve"
 mod_config="/etc/modprobe.d/zfs.conf"
 onetb=1099511627776
@@ -22,30 +25,23 @@ function msg {
     if [[ -z $2 ]]; then return; fi
     if [[ $1 == "info" ]]; then
         prefix=" (i)"
-        if [[ -z $3 ]]; then
-            printf "$prefix $2\n"
-        else
-            printf "$prefix %-49s $3\n" "$2"
-        fi
-    elif [[ $1 == "check" ]]; then
-        printf " (?) %-48s" "$2"
-    elif [[ $1 == "ack" ]]; then
-        echo -e "  [+] $2"
-    elif [[ $1 == "error" ]]; then
-        echo " /!\ $2"
+
+        if [[ -z $3 ]]; then printf "$prefix $2\n"
+            else printf "$prefix %-49s $3\n" "$2"
+            fi
+
+    elif [[ $1 == "check"   ]]; then printf " (?) %-48s" "$2"
+    elif [[ $1 == "ack"     ]]; then printf "  [+] $2\n"
+    elif [[ $1 == "error"   ]]; then printf " /!\ $2\n"
     elif [[ $1 == "warning" ]]; then
         prefix=" [*]"
-        if [[ -z $3 ]]; then
-            printf "$prefix $2\n"
-        else
-            printf "$prefix %-49s $3\n" "$2"
-        fi
-    elif [[ $1 == "title" ]]; then
-        printf "\n -[$2]--\n\n"
-    elif [[ $1 == "credits" ]]; then
-        printf "%-17s geoai777@gmail.com 2024\n\n" " "
-    else
-        return
+        if [[ -z $3 ]]; then printf "$prefix $2\n"
+            else printf "$prefix %-49s $3\n" "$2"
+            fi
+
+    elif [[ $1 == "title"   ]]; then printf "\n -[$2]--\n\n"
+    elif [[ $1 == "credits" ]]; then printf "%-17s geoai777@gmail.com 2024\n\n" " "
+    else return
     fi
 }
 
@@ -53,16 +49,14 @@ function divider {
 #
 # Render simple divider, to improve ergonomics
 #
-    if [[ -z $1 ]]; then
-        divider_char="-"
-    else
-        # only first character will be used no matter what.
-        divider_char=$(printf %.1s "$1")
-    fi
 
-    for i in $(seq 1 `tput cols`); do
-        printf $divider_char
-    done
+    # only first character will be used no matter what.
+    if [[ -z $1 ]]; then divider_char="-"
+        else divider_char=$(printf %.1s "$1")
+        fi
+
+    for i in $(seq 1 `tput cols`); do printf $divider_char; done
+
     printf "\n"
 }
 
@@ -76,33 +70,25 @@ divider
 #
 msg check "bash version is sufficient"
 bv="${BASH_VERSION:-0}"
-if [[ `echo $bv | awk -F. '{print $1}'` -ge 4 ]]; then
-    msg ack "it is"
-else
-    msg error "Bash version should be above 4"
-fi
+if [[ `echo $bv | awk -F. '{print $1}'` -ge 4 ]]; then msg ack "it is"
+    else msg error "Bash version should be above 4"
+    fi
 
 #
 # check script runs as root
 #
 msg check "are you root?"
-if [[ `whoami` == 'root' ]]; then
-    msg ack "I. Am. ROOT! :)"
-else
-    msg error "this script should run as root"
-    exit 1
-fi
+if [[ `whoami` == 'root' ]]; then msg ack "I. Am. ROOT! :)"
+    else msg error "this script should run as root"; exit 1
+    fi
 
 #
 # check we're running on proxmox (might write for common zfs later)
 #
 msg check "check if system is proxmox"
-if [[ -d $pve_dir ]]; then
-    msg ack "it is"
-else
-    msg error "This is not the prox mox we are looking for..."
-    exit 1
-fi
+if [[ -d $pve_dir ]]; then msg ack "it is"
+    else msg error "This is not the prox mox we are looking for..."; exit 1
+    fi
 
 divider
 #
@@ -162,14 +148,13 @@ while true; do
     msg info "Enter ARC max RAM size here. Valid options are:"
     msg info "  - 4G, 100M, 1T, 32768" "enter in any format"
     msg info "  - a - set recommended size, that is:" "$cache_recommend"
-    msg check "  - Ctrl+C - exit"
+    msg check "  - Ctrl+C or q - exit"
     read new_size_human
-    if [[ $new_size_human =~ $re ]]; then
-        break
-    elif [[ $new_size_human -eq "a" ]]; then
-        new_size_human=$cache_recommend
-        break
-    fi
+
+    if [[ $new_size_human =~ $re ]]; then break
+        elif [ $new_size_human = "a" ]; then new_size_human=$cache_recommend; break
+        elif [ $new_size_human = "q" ]; then exit 1
+        fi
 done
 
 new_size=`numfmt --from=iec $new_size_human`
